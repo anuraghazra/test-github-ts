c1f23206b31f464eecc6aa7adc7e99833aa264cf
Anurag <hazru.anurag@gmail.com>
2022-09-27 17:52:10 +0530

    chore changes

--- packages/one/utils/sayHello.ts
+++ packages/one/utils/sayHello.ts
@@ -1,4 +1,8 @@
 import isOdd from "is-odd";
+import { MyTypeInAnotherPackage } from "another";
+import { HelloFromAnotherFile } from "second-module";
+
+type K = MyTypeInAnotherPackage | HelloFromAnotherFile;
 
 export const checkOdd = (value: number) => {
   return isOdd(value);
